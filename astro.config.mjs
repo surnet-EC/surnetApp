b4f49ca91a7e893ed2f4f5b8e200d6b3bbbab1cc
surnet-EC <clientesurnet@gmail.com>
2024-09-11 08:59:08 -0500

    Add new yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://surnet-EC.github.io',
-  base: 'surnetApp',
   integrations: [tailwind()]
 });
