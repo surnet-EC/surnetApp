ee20e88c72b45622c637f6006872432de007d53b
surnet-EC <clientesurnet@gmail.com>
2024-09-11 11:31:55 -0500

    Redireccionando para despligue en github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://surnet-EC.github.io',
+  site: 'https://surnet-EC.github.io/surnetApp',
   integrations: [tailwind()]
 });
